18b30aa7f92a1c3fa04c30174664d145cfd350a5
ygorkagrishin <webygorkagrishin@gmail.com>
2018-05-02 14:08:17 +0300

    On scripts

--- src/static/script/orient.js
+++ src/static/script/orient.js
@@ -1,43 +1,43 @@
-// window.addEventListener('orientationchange', function() {
-//     var orientation = screen.orientation || screen.msOrientation || screen.mozOrientation,
-//         err = document.querySelector('.page__landscape-err');
+window.addEventListener('orientationchange', function() {
+    var orientation = screen.orientation || screen.msOrientation || screen.mozOrientation,
+        err = document.querySelector('.page__landscape-err');
     
-//     if (!orientation) return;
+    if (!orientation) return;
 
-//     switch(orientation.angle) {
-//         case 0:
-//             /* 
-//                 Если не произошло поворота устройства или 
-//                 устройство вернулось в исходную точку в 0 градусов, 
-//                 у элемента удаляется класс active.
-//             */
-//             if ( err.classList.contains('active') ) {
-//                 err.classList.remove('active');
+    switch(orientation.angle) {
+        case 0:
+            /* 
+                Если не произошло поворота устройства или 
+                устройство вернулось в исходную точку в 0 градусов, 
+                у элемента удаляется класс active.
+            */
+            if ( err.classList.contains('active') ) {
+                err.classList.remove('active');
 
-//                 SCROLL = true;
-//             }
-//             break; 
-//         case 90:
-//             /* 
-//                 Если происходит поворот устройства на 90 градусов, 
-//                 элементу устанавливается класс active.
-//             */
-//             if ( !err.classList.contains('active') ) {
-//                 err.classList.add('active');
+                SCROLL = true;
+            }
+            break; 
+        case 90:
+            /* 
+                Если происходит поворот устройства на 90 градусов, 
+                элементу устанавливается класс active.
+            */
+            if ( !err.classList.contains('active') ) {
+                err.classList.add('active');
 
-//                 SCROLL = false;
-//             }
-//             break;
-//         case -90: 
-//             /* 
-//                 Если происходит поворот устройства на -90 градусов, 
-//                 элементу устанавливается класс active.
-//             */
-//             if ( !err.classList.contains('active') ) { 
-//                 err.classList.add('active');
+                SCROLL = false;
+            }
+            break;
+        case -90: 
+            /* 
+                Если происходит поворот устройства на -90 градусов, 
+                элементу устанавливается класс active.
+            */
+            if ( !err.classList.contains('active') ) { 
+                err.classList.add('active');
 
-//                 SCROLL = false;
-//             }
-//             break;
-//     }
-// });
+                SCROLL = false;
+            }
+            break;
+    }
+});
